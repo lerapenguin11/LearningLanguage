2d63dc1747484a4dc030d9036255732d54b30fa0
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2024-02-19 23:24:50 +0500

    add modules navigation fragment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,12 @@ dependencyResolutionManagement {
 
 rootProject.name = "LearningLanguage"
 include(":app")
- 
+include(":feature_word:word_data")
+include(":feature_word:word_domain")
+include(":feature_study:study_data")
+include(":feature_study:study_domain")
+include(":feature_study:study_presentation")
+include(":feature_lists:lists_data")
+include(":feature_lists:lists_domain")
+include(":feature_lists:lists_presentation")
+include(":feature_word:word_presentation")
